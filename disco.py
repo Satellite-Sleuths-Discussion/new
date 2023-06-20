ef96810f025b0a7b61e30054fb0dce6380695d4b
aadityapuri <66025006+aadityapuri@users.noreply.github.com>
2023-06-20 23:15:16 +0530

    Delete disco.py

--- disco.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-#Stuti can't commit
-# Heyyyy
-#hiii
-# Stuti can commit now ;)
-#cauz we encouraged stuti enough and asked him(git) to allow her to commit. 
